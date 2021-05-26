e073c26c0578215088d579dbcd7f866500422af4
Byron <byronhsu1230@gmail.com>
2021-05-26 13:48:02 +0800

    finish 7

--- hw1/q7_imdb_250.sql
+++ hw1/q7_imdb_250.sql
@@ -1,2 +1,16 @@
-SELECT primary_title, (COUNT(votes)*1.0/(COUNT(votes)*1.0+25000))*AVG(rating)+(25000/(COUNT(votes)*1.0+25000))*(SELECT AVG(rating) FROM ratings) AS wr FROM titles INNER JOIN ratings ON titles.title_id = ratings.title_id GROUP BY
-titles.title_id ORDER BY wr DESC LIMIT 10;
+WITH 
+    weightedAverageRating (war) AS (
+        SELECT SUM(votes * rating) / SUM(votes) 
+        FROM ratings
+        INNER JOIN titles
+        ON ratings.title_id == titles.title_id and titles.type == "movie"
+    ),
+    minimumVotes (mv) AS (
+        SELECT 25000.0
+    )
+SELECT primary_title, (votes/(votes+mv)) * (rating) + (mv/(votes+mv)) * war as wr -- title_id is PK in ratings, so we do not need to group by title_id
+FROM ratings, minimumVotes, weightedAverageRating
+INNER JOIN titles
+ON ratings.title_id == titles.title_id and titles.type == "movie"
+ORDER BY wr DESC
+LIMIT 250;
